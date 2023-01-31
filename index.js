51228b065e3b070b2c7b7a41828ddf828f12063d
burakdiker <burak.diker@ug.bilkent.edu.tr>
2023-01-31 09:34:42 +0300

    mongo connection arranged

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 const express = require('express');
-const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index');
 const instructorRouter = require('./routes/instructors');
 const studentRouter = require('./routes/students');
-const mongoose = require('mongoose');
+require('./mongo-connection');
 
 const app = express();
-dotenv.config();
-
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log('MongoDB connected'))
-  .catch((e) => console.log(e));
 
 //middleware
 app.use(bodyParser.json());
